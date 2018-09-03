627bff3a5c7e43642d267fd9a8a355bbcef136c1
Kleber Manoel Infante <corujito@gmail.com>
2018-09-03 11:37:00 -0400

    rest authentication

--- app/services/champz_service.rb
+++ app/services/champz_service.rb
@@ -5,8 +5,8 @@ class ChampzService
   def get_season_summary(season_id)
     service_host = ENV['CHAMPZ_RESTSERVICE_SERVICE_SERVICE_HOST'] || "localhost"
     service_port = ENV['CHAMPZ_RESTSERVICE_SERVICE_SERVICE_PORT'] || "8080"
-    user = ENV['CHAMPZ_RESTSERVICE_SERVICE_SERVICE_USER'] || "user"
-    password = ENV['CHAMPZ_RESTSERVICE_SERVICE_SERVICE_PWD'] || "password"
+    user = ENV['CHAMPZ_RESTSERVICE_USERNAME'] || "user"
+    password = ENV['CHAMPZ_RESTSERVICE_PASSWORD'] || "password"
     response = RestClient.get "http://#{user}:#{password}@#{service_host}:#{service_port}/api/seasonsSummary/" + season_id
     JSON.parse(response.body, object_class: OpenStruct)
   end
